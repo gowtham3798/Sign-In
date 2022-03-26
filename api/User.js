e67faab92525ea94dd87fa8f8078a35f6bf3cf63
gowtham <gowtham3798@gmail.com>
2022-03-26 22:43:33 +0530

    email verification

--- api/User.js
+++ api/User.js
@@ -1,9 +1,33 @@
+require("dotenv").config()
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const User = require('./../models/User')
+const UserVerification = require('./../models/userVerification')
+const nodemailer = require('nodemailer');
+const {v4 : uuidv4} = require('uuid')
 
-console.log(new Date())
+
+
+
+let transporter = nodemailer.createTransport({
+    service :'gmail',
+    auth : {
+        user : process.env.AUTH_EMAIL,
+        pass : process.env.AUTH_PASS
+    }
+})
+
+transporter.verify((error,success) => {
+    if(error) {
+        console.log(error)
+    }
+    else{
+        console.log('Ready for message')
+        console.log(success)
+    }
+})
+
 
 router.post('/signup',(req,res) => {
     let {name,email,password,dateOfBirth} = req.body;
@@ -59,15 +83,13 @@ router.post('/signup',(req,res) => {
                     name,
                     email,
                     password : hashedPassword,
-                    dateOfBirth
+                    dateOfBirth,
+                    verified: false
                 })
                 console.log(newUser)
+
                 newUser.save().then((result) => {
-                    res.json({
-                        status : 'SUCCESS',
-                        message : 'Signup Successful!',
-                        data : result
-                    })
+                    sendVerificationEmail(result,res)
                 })
                 .catch((error) => {
                     console.error(error)
@@ -95,6 +117,154 @@ router.post('/signup',(req,res) => {
     }
 })
 
+
+
+//emailverification
+const sendVerificationEmail = ({_id,email},res) =>{
+ const currentUrl = 'http://localhost:3200/'
+ 
+ const uniqueString = uuidv4() + _id;
+
+ const mailOptions = {
+     from : process.env.AUTH_EMAIL,
+     to : email,
+     subject : 'Verify Your Email',
+     html : `<p>Verify your email to complete the signup and login to your account.</p>
+             <p>This link<b>link expires in 6 hours.</b></p>
+             <p>Press <a href=${currentUrl+"user/verify"+_id+"/"+uniqueString}>here</a>to proceed.</p>` 
+ }
+ 
+ //Hashing uniqueString
+ const saltRounds = 10;
+  bcrypt
+  .hash(uniqueString,saltRounds)
+  .then((hashedUniqueString) => {
+      const newVerification = new UserVerification({
+          userId : _id,
+          uniqueString : hashedUniqueString,
+          createdAt : Date.now(),
+          expiresAt : Date.now() + 2600000
+      })
+
+      newVerification
+      .save()
+      .then(() => {
+        transporter
+        .sendMail(mailOptions)
+        .then(() => {
+                res.json({
+                    status : 'PENDING',
+                    message : "Verification mail sent"
+                })
+            })
+            .catch(err => {
+                res.json({
+                    status : 'FAILED',
+                    message : "Couldn't send verification email address"
+                })
+            })
+            })
+      .catch((error) => {
+        res.json({
+            status : 'FAILED',
+            message : "Couldn't save verification email address"
+        })
+      })
+  })
+  .catch(error => {
+    res.json({
+        status : 'FAILED',
+        message : 'Error occured while hashing uniqueString'
+    })
+  })
+
+}
+
+
+
+router.get('/verify/:userId/:uniqueString',(req,res)=>{
+    let {userId,uniqueString} = req.params;
+
+    UserVerification
+    .find({userId})
+    .then((result) => {
+        if(result.length>0){
+             const {expiresAt} = result[0];
+             const hashedUniqueString = result[0].uniqueString;
+             
+             if (expiresAt < Date.now()) {
+                 UserVerification
+                 .deleteOne({userId})
+                 .then((result) => {
+                    User
+                    .deleteOne({_id:userId})
+                    .then(() => {
+                        res.json({
+                            status : 'FAILED',
+                            message : 'An error has occurred ehile checking existing user record'
+                        })
+                    })
+                    .catch(error => {
+                        console.log(error)
+                        res.json({
+                            status : 'FAILED',
+                            message : 'Clearing User with expired uniquestring failed'
+                        })
+                    })
+                 })
+                 .catch(error => {
+                     res.json({
+                         status : 'FAILED',
+                         message : "Link had been expired"
+                     })
+                 })
+             }
+             else{
+                 bcrypt.compare(uniqueString,hashedUniqueString)
+                 .then((result) => {
+                    if(result){
+                        User
+                        .updateOne({_id:userId},{verified:true})
+                        .then(() => {
+                            UserVerification
+                            .deleteOne({userId})
+                            .then((result) => {
+                                console.log(result)
+                            })
+                            .catch(error => {
+                                console.log(error)
+                            })
+                        })
+                        .catch(error => {
+                            console.log(error)
+                        })
+                    } 
+
+                 })
+                 .catch(error => {
+                     console.log(error)
+                 })
+             }
+     
+
+        }else{
+            console.log("Account already Exists")
+        }
+    })
+    .catch((err) => {
+        console.log(err)
+        res.json({
+            status : 'FAILED',
+            message : 'An error occurred while checking for existing user record'
+        })
+    })
+})
+
+
+
+
+
+//Signin
 router.post('/signin',(req,res) => {
     // let {email,password} = req.body;
     let email = req.body.email;
@@ -109,30 +279,39 @@ router.post('/signin',(req,res) => {
     }
     else(User.find({email}))
     .then(data => {
-    if(data)
+    if(data.length)
     {
-        const hashedPassword = data[0].password;
-        bcrypt.compare(password,hashedPassword).then((result) => {
-            if(result){
-                res.json({
-                    status : "SUCCESS",
-                    message : "Signin successful!",
-                    data : data
-                })
-            }
-            else {
-                res.json({
-                    status : 'FAILED',
-                    message : 'Incorrect password'
-                })
-            }
-        }).catch((error) => {
+         
+        if(!data[0].length){
             res.json({
                 status : 'FAILED',
-                message : 'Error occured while comparing password'
+                message : "Email hasn't been verified yet.check your inbox."
             })
-        })
-    }
+        }
+        else{
+            const hashedPassword = data[0].password;
+            bcrypt.compare(password,hashedPassword).then((result) => {
+                if(result){
+                    res.json({
+                        status : "SUCCESS",
+                        message : "Signin successful!",
+                        data : data
+                    })
+                }
+                else {
+                    res.json({
+                        status : 'FAILED',
+                        message : 'Incorrect password'
+                    })
+                }
+            }).catch((error) => {
+                res.json({
+                    status : 'FAILED',
+                    message : 'Error occured while comparing password'
+                })
+            })
+        }    
+        }
     else{
         res.json({
             status : 'FAILED',
--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const UserSchema = new schema({
     name : String,
     email : String,
     password : String,
-    dateOfBirth : String
+    dateOfBirth : String,
+    verified : Boolean,
 })
 
 const User = mongoose.model('User',UserSchema)
